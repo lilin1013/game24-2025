d57267683fa06c0836cbb07447ccc05c2de57137
Lin Li <lin.li.10@volvocars.com>
2023-02-13 18:58:21 +0100

    add key for pockerDeck

--- src/components/pokerDeck.tsx
+++ src/components/pokerDeck.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from './card';
+import { v4 as uuidv4 } from 'uuid';
 
 interface Props {
   value?: Array<string>;
@@ -7,9 +8,10 @@ interface Props {
 
 const PokerDeck: React.FC<Props> = ({  value }) => {
   value = value || ['','','',''];
+  const uuid = uuidv4();
   return (
     <div className="flex sm:gap-4 justify-between gap-1 sm: w-9/12 w-11/12">
-        {value.map((cardValue) =><Card value={cardValue}></Card>)}
+        {value.map((cardValue) =><Card key={uuid} value={cardValue}></Card>)}
     </div>
   );
 };
