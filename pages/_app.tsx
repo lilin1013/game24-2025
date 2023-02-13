1e4dac15da110fbe181b6a116e653d58422872dc
Lin Li <lin.li.10@volvocars.com>
2023-02-13 22:38:06 +0100

    name change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Game 24</title>
+        <title>Math 24</title>
         <meta name="description" content="online math game" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
