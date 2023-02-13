62079d3a65bffaa3e7588b4aec1fbcdd5d01e705
Lin Li <lin.li.10@volvocars.com>
2023-02-13 19:03:48 +0100

    fix

--- src/components/pokerDeck.tsx
+++ src/components/pokerDeck.tsx
@@ -8,10 +8,9 @@ interface Props {
 
 const PokerDeck: React.FC<Props> = ({  value }) => {
   value = value || ['','','',''];
-  const uuid = uuidv4();
   return (
     <div className="flex sm:gap-4 justify-between gap-1 sm: w-9/12 w-11/12">
-        {value.map((cardValue) =><Card key={uuid} value={cardValue}></Card>)}
+        {value.map((cardValue) =><Card key={uuidv4()} value={cardValue}></Card>)}
     </div>
   );
 };
